3ec7640f7933ecb7da519474d4c9c1bcf07fcb76
FabrizioCoder <fabrizioivan36@gmail.com>
2024-01-25 14:32:44 -0600

    Update background color and logo size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,32 @@
 import Image from 'next/image';
-// TODO: Make responsive
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-normal text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          By Fabrizio Santana & FreeAoi
+    <main className="flex flex-col items-center justify-between min-h-screen p-4 lg:p-12">
+      <div className="z-10 flex flex-col items-center justify-between w-full max-w-5xl font-normal text-sm lg:flex-row">
+        <div className="fixed bottom-0 left-0 flex items-end justify-center w-full h-24 bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          By fbrz.sf, FreeAoi & MARCROCK22
         </div>
       </div>
 
       <Image
-        className="relative dark:drop-shadow-[0_0_0.5rem_#ffffff70] border rounded-full"
+        className="relative border rounded-full dark:drop-shadow-[0_0_0.5rem_#ffffff70]"
         src="/logo.svg"
         alt="Luxanna Logo"
-        width={200}
-        height={200}
+        width={280}
+        height={280}
         priority
-      ></Image>
+      />
+      <Image
+        className="relative dark:drop-shadow-[0_0_0.5rem_#ffffff70]"
+        src="/luxanna.svg"
+        alt="Luxanna"
+        width={300}
+        height={300}
+        priority
+      />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1] top-2">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.5rem_#ffffff70]"
-          src="/luxanna.svg"
-          alt="Luxanna"
-          width={300}
-          height={300}
-          priority
-        />
-      </div>
-
-      <div className="relative flex place-items-center lg:text-center">
+      <div className="relative flex flex-col items-center space-y-4 lg:space-y-0 lg:flex-row lg:space-x-4 lg:text-center">
         <a
           href="https://discord.com/oauth2/authorize?client_id=949565943275720736&permissions=2147796992&scope=bot%20applications.commands"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"

dad37e7fcbdbd41126994fbe45c2f8d8bb945bf1
FabrizioCoder <fabrizioivan36@gmail.com>
2024-01-25 13:34:23 -0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,69 @@
+import Image from 'next/image';
+// TODO: Make responsive
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-12">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-normal text-sm lg:flex">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          By Fabrizio Santana & FreeAoi
+        </div>
+      </div>
+
+      <Image
+        className="relative dark:drop-shadow-[0_0_0.5rem_#ffffff70] border rounded-full"
+        src="/logo.svg"
+        alt="Luxanna Logo"
+        width={200}
+        height={200}
+        priority
+      ></Image>
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1] top-2">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.5rem_#ffffff70]"
+          src="/luxanna.svg"
+          alt="Luxanna"
+          width={300}
+          height={300}
+          priority
+        />
+      </div>
+
+      <div className="relative flex place-items-center lg:text-center">
+        <a
+          href="https://discord.com/oauth2/authorize?client_id=949565943275720736&permissions=2147796992&scope=bot%20applications.commands"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Invite me{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Invite me to your server to get vital information to play
+          </p>
+        </a>
+
+        <a
+          href="#"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_parent"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Dashboard{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            View, add or remove your League of Legends account in the bot
+          </p>
+        </a>
+      </div>
+    </main>
+  );
+}
